80bffe7d25e0f9029b57e4a6ab0b9019296fe249
Tshirt-hfk <fanglei212@outlook.com>
2020-08-17 20:40:58 +0800

    设置默认分类

--- src/views/illegalFund.vue
+++ src/views/illegalFund.vue
@@ -116,6 +116,7 @@ export default {
     };
   },
   mounted() {
+    this.$refs.tree.setChecked(1, true, true)
     this.nodes = this.$refs.tree.getCheckedNodes(true, false).map((item) => {
       return item.label;
     });
